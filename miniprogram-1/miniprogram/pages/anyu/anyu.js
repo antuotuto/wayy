0b076a39f3fad5e18495b14d9aa327f67d055871
Anan <675988829@qq.com>
2020-12-01 17:36:01 +0800

    1.2.0

--- miniprogram-1/miniprogram/pages/anyu/anyu.js
+++ miniprogram-1/miniprogram/pages/anyu/anyu.js
@@ -1,4 +1,6 @@
-const db=wx.cloud.database()
+// pages/anyu/anyu.js
+
+const db=wx.cloud.database();
 
 Page({
 
@@ -9,15 +11,15 @@ Page({
     dataObj:""
   },
 
-  // 查询数据
+  // 加载初始数据
   getData(){
-
-    db.collection("list").where({
-      author:"杨羽"
-    }).get().then(res=>{
-      dataObj:res.data
+    db.collection("list")
+    .get()
+    .then(res=>{
+      this.setData({
+        dataArr:res.data
+      })
     })
-
   },
 
   // 添加数据
@@ -29,11 +31,9 @@ Page({
     db.collection("list").add({
       data:{
         title:"测试标题",
-        author:"孙红",
         content:"阿斯顿发所大所大所大所大所大所多萨达所大所多"
       }
     }).then(res=>{
-      console.log(res)
       wx.hideLoading()
     })
   },
@@ -49,18 +49,6 @@ Page({
 
   },
 
-  // 加载初始数据
-  getData(){
-    db.collection("list")
-    .get()
-    .then(res=>{
-      this.setData({
-        dataArr:res.data
-      })
-    })
-  },
-
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -72,48 +60,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
